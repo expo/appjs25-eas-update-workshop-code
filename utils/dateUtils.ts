8183e63944f20ac362fbcc412295fc44e07345e6
Quinlan Jung <quin@expo.io>
2025-05-18 23:24:12 -0700

    date utils

---
+++ utils/dateUtils.ts
@@ -0,0 +1,9 @@
+// Date-time utils for the persistent date hook and for date comparison
+
+export const date1GreaterThanDate2 = (
+  date1: Date | undefined,
+  date2: Date | undefined
+) => dateToTimeInMilliSeconds(date1) > dateToTimeInMilliSeconds(date2);
+
+const dateToTimeInMilliSeconds = (date: Date | undefined) =>
+  date ? Math.floor(date.getTime()) : -1;
