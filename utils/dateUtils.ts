64615c7aa44cbe886e2b99c253d11327747334e9
Quinlan Jung <quin@expo.io>
2025-05-20 01:36:38 -0700

    Milestone 3: interval check

--- utils/dateUtils.ts
+++ utils/dateUtils.ts
@@ -7,3 +7,8 @@ export const date1GreaterThanDate2 = (
 
 const dateToTimeInMilliSeconds = (date: Date | undefined) =>
   date ? Math.floor(date.getTime()) : -1;
+
+export const dateDifferenceInMilliSeconds = (
+  date1: Date | undefined,
+  date2: Date | undefined,
+) => dateToTimeInMilliSeconds(date1) - dateToTimeInMilliSeconds(date2);
