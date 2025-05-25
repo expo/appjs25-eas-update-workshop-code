4aa6948bfb0c264cedda2ad271bb24dd5bb3e3d1
Quinlan Jung <quin@expo.io>
2025-05-25 18:31:39 +0200

    Milestone 2b: Configure app namespace
    change deeplink

---
+++ app.config.js
@@ -0,0 +1,45 @@
+module.exports = () => {
+  const appVariant = process.env.APP_VARIANT || "development";
+  return {
+    expo: {
+      name: "Art Museum",
+      slug: "appjs25-update-workshop-code",
+      version: "1.0.0",
+      orientation: "portrait",
+      icon:
+        appVariant === "development"
+          ? "./assets/images/icon.png"
+          : "./assets/images/preview-icon.png",
+      scheme: appVariant === "development" ? "myappdev" : "myapp",
+      userInterfaceStyle: "automatic",
+      splash: {
+        image: "./assets/images/splash.png",
+        resizeMode: "contain",
+        backgroundColor: "#ffffff",
+      },
+      assetBundlePatterns: ["**/*"],
+      ios: {
+        supportsTablet: true,
+        // bundleIdentifier: `com.expo.appjs25updateworkshopcode.[your-username].${appVariant}`,
+      },
+      android: {
+        adaptiveIcon: {
+          foregroundImage: "./assets/images/adaptive-icon.png",
+          backgroundColor: "#ffffff",
+          // package: `com.expo.appjs25updateworkshopcode.[your-username].${appVariant}`,
+        },
+      },
+      web: {
+        bundler: "metro",
+        favicon: "./assets/images/favicon.png",
+      },
+      plugins: [["expo-router"], "expo-font", "expo-web-browser"],
+      experiments: {
+        typedRoutes: true,
+      },
+      runtimeVersion: {
+        policy: "appVersion",
+      },
+    },
+  };
+};
